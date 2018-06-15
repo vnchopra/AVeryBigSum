31873404e4346b4ee9abb085de19fdab8ad0ae89
Varun Chopra <vnchopra@gmail.com>
2018-06-14 20:04:17 -0700

    Updated formatting.

--- AVeryBigSum.java
+++ AVeryBigSum.java
@@ -4,9 +4,11 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class AVeryBigSum {
+public class AVeryBigSum 
+{
 
-	static long aVeryBigSum(int n, long[] ar) {
+	static long aVeryBigSum(int n, long[] ar) 
+	{
 		long theresult = 0;
 		
 		AVeryBigSum theObj = new AVeryBigSum();
@@ -15,11 +17,13 @@ public class AVeryBigSum {
 		return theresult;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         long[] ar = new long[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
+        for(int ar_i = 0; ar_i < n; ar_i++)
+        {
             ar[ar_i] = in.nextLong();
         }
         long result = aVeryBigSum(n, ar);
